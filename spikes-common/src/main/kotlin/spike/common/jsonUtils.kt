1419a968e27da9f8044982b474a15c20f8588772
Pawel Marks <paweljmarks@gmail.com>
2017-05-03 22:03:39 +0200

    Deserialization shortcut added

--- spikes-common/src/main/kotlin/spike/common/jsonUtils.kt
+++ spikes-common/src/main/kotlin/spike/common/jsonUtils.kt
@@ -1,7 +1,14 @@
 package spike.common
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
 fun Any.toJson(): String {
     return jacksonObjectMapper().writeValueAsString(this)
+}
+
+inline fun <reified T: Any> String.read(): T {
+    return jacksonObjectMapper().readValue<T>(this)
 }
