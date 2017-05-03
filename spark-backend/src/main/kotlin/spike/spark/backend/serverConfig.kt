f26dfff5d627a8287d2336e2f629ddd9f1c784f6
Pawel Marks <paweljmarks@gmail.com>
2017-05-03 16:42:15 +0200

    Place for backend data added

--- spark-backend/src/main/kotlin/spike/spark/backend/serverConfig.kt
+++ spark-backend/src/main/kotlin/spike/spark/backend/serverConfig.kt
@@ -1,9 +1,11 @@
 package spike.spark.backend
 
 import spark.Spark.*
+import spike.common.Person
 
 fun main(args: Array<String>) {
+    staticFiles.location("/runtime-data/images")
     get("/") { _, _ ->
-        "WORKING!!!"
+        Person("Szkocik", 100)
     }
 }
